0b5ff58d4ad934479b4dc91404b53e8372f5cecb
enchantuer <31900604+enchantuer@users.noreply.github.com>
2023-11-21 16:42:17 +0100

    Initial files for the Graph class

---
+++ Graph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Isen-Ouest on 17/11/2023.
+//
+
+#include "Graph.h"
---
+++ Graph.h
@@ -0,0 +1,14 @@
+//
+// Created by Isen-Ouest on 17/11/2023.
+//
+
+#ifndef PROJET_MBP_GRAPH_H
+#define PROJET_MBP_GRAPH_H
+
+
+class Graph {
+
+};
+
+
+#endif //PROJET_MBP_GRAPH_H
