da105114e7e08b6e4aef952e87d3beb9061cc5d9
enchantuer <31900604+enchantuer@users.noreply.github.com>
2023-12-01 17:03:27 +0100

    compare constructive and exact

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@ int main() {
     }
     cout << endl;
     // Graph 2 (random)
+    cout << endl << "Random graph" << endl;
     Graph g2(10, 0.25);
     cout << "Graph 2 (n=5, d=0.3)" << endl;
     cout << "Number of edges : " << g2.getM() << endl;
@@ -39,7 +40,9 @@ int main() {
         cout << v << " ";
     }
     cout << endl;
+
     // Test constructive heuristic
+    cout << endl << "Constructive heuristic" << endl;
     Graph g3(10, 0.3);
     cout << "Graph 3 (n=10, d=0.3)" << endl;
     cout << "Number of edges : " << g3.getM() << endl;
@@ -57,10 +60,10 @@ int main() {
     cout << "Number of edges linking the two groups : " << g3.getNumberOfEdgesLinkingTwoGroups(result[0], result[1]) << endl;
 
     // Test exact algorithm
+    cout << endl << "Exact algorithm" << endl;
     Graph g4(10, 0.3);
     cout << "Graph 4 (n=10, d=0.3)" << endl;
     cout << "Number of edges : " << g4.getM() << endl;
-    vector<vector<int>> temp(2);
     vector<vector<int>> result2 = g4.exactAlgorithm();
     cout << "Group 1 : ";
     for (auto v : result2[0]){
@@ -73,6 +76,39 @@ int main() {
     }
     cout << endl;
     cout << "Number of edges linking the two groups : " << g4.getNumberOfEdgesLinkingTwoGroups(result2[0], result2[1]) << endl;
+
+    // Compare exact algorithm and constructive heuristic
+    cout << endl << "Compare exact algorithm and constructive heuristic" << endl;
+    Graph g5(20, 0.9);
+    cout << "Graph 5 (n=20, d=0.3)" << endl;
+    cout << "Number of edges : " << g5.getM() << endl;
+    cout << "Exact algorithm" << endl;
+    vector<vector<int>> result3 = g5.exactAlgorithm();
+    cout << "Group 1 : ";
+    for (auto v : result3[0]){
+        cout << v << " ";
+    }
+    cout << endl;
+    cout << "Group 2 : ";
+    for (auto v : result3[1]){
+        cout << v << " ";
+    }
+    cout << endl;
+    cout << "Number of edges linking the two groups : " << g5.getNumberOfEdgesLinkingTwoGroups(result3[0], result3[1]) << endl;
+    cout << "Constructive heuristic" << endl;
+    vector<vector<int>> result4 = g5.constructiveHeuristic();
+    cout << "Group 1 : ";
+    for (auto v : result4[0]){
+        cout << v << " ";
+    }
+    cout << endl;
+    cout << "Group 2 : ";
+    for (auto v : result4[1]){
+        cout << v << " ";
+    }
+    cout << endl;
+    cout << "Number of edges linking the two groups : " << g5.getNumberOfEdgesLinkingTwoGroups(result4[0], result4[1]) << endl;
+
 
     return 0;
 }
